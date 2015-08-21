9edd1fbd64583dcc5197ef2322b0d4dac16cd7e1
Andrew Thomson <athomsonguy@gmail.com>
2015-08-20 21:44:45 -0700

    fix version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='esmgr',
-      version='1.0',
+      version='0.1',
       description='Tool for managing elastic search clusters.',
       url='https://github.com/Livefyre/elasticsearch-mgr',
       author='andrew thomson',
